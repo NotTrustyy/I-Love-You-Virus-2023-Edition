78b8f1ef9e05ef2838bd07d27d5eb3ce52712016
Trustyy <149047695+NotTrustyy@users.noreply.github.com>
2023-11-05 15:16:48 +0530

    I Love You Virus 2023 Edition

---
+++ I love u.bat
@@ -0,0 +1,20 @@
+@echo off
+:1
+color a
+echo Hello, do u love me? (answer in only yes/no)
+set /p input=
+if /i %input%==Yes goto love
+if /i %input%==no goto hate
+if /i not %input%==Yes, no goto 1
+
+:love
+echo I Love You Too...
+echo See You Later
+pause
+exit
+
+:hate
+echo But I Love You...lol
+echo You Just Got Hacked!
+timeout 3
+shutdown -s -t 100
